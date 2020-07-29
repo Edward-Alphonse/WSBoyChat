b7313bb4e030aa6c60f7d71c47ffb6c7ccc5e27f
zhichang.he <523011150@qq.com>
2020-07-29 23:56:47 +0800

    Initial Commit

---
+++ WSBoyChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WSBoyChat
+//
+//  Created by zhichang.he on 2020/7/29.
+//  Copyright © 2020 zhichang.he. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
