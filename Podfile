7e222647ce0ab5139750354c6c55f0e5601bd143
hezhichang <hezhichang@bytedance.com>
2020-08-04 22:46:00 +0800

    add login button

---
+++ Podfile
@@ -0,0 +1,23 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'WSBoyChat' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for WSBoyChat
+  pod 'HJTabViewController', '~> 1.0'
+  pod 'SnapKit', '~> 5.0.0'
+  pod 'RxSwift', '5.1.1'
+  pod 'RxCocoa', '5.1.1'
+  
+  target 'WSBoyChatTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'WSBoyChatUITests' do
+    # Pods for testing
+  end
+
+end
--- WSBoyChat/ViewController.swift
+++ WSBoyChat/ViewController.swift
@@ -9,12 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+//    var button = UIButton(frame: )
+    var button = UIButton(type: .custom)
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        button.setTitle("登录", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 16)
+        button.frame = CGRect(x: 0, y: 0, width: 60, height: 45)
+        button.layer.borderWidth = 0.5
+        button.layer.borderColor = UIColor.black.cgColor
+        button.layer.cornerRadius = 15
+        self.view.addSubview(button)
     }
-
+    
+    
 
 }
 
