3e16168546b0d08360c0e6b44b8b2500c3a31569
hezhichang <hezhichang@bytedance.com>
2020-08-26 22:03:36 +0800

    network

---
+++ WSBoyChat/FSUIKit/FSArrowView.swift
@@ -0,0 +1,21 @@
+//
+//  FSArrowView.swift
+//  WSBoyChat
+//
+//  Created by hezhichang on 2020/8/26.
+//  Copyright © 2020 zhichang.he. All rights reserved.
+//
+
+import UIKit
+
+class FSArrowView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
